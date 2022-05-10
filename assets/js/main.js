5862432a34928c2f9e92bbfdf89d4cc5674f404e
EliasFredoni <fredoni.elias@gmail.com>
2022-05-10 16:11:22 +0200

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+
+let vorname = document.getElementById("vorname");
+let nachname = document.getElementById("nachname");
+let land = document.getElementById("land");
+
+function button() {
+    console.log(vorname.value);
+    console.log(nachname.value);
+    console.log(land.value);
+}
