6f187d5db2c6c0db5306d9df0db44f838026698b
EliasFredoni <fredoni.elias@gmail.com>
2022-05-31 13:58:17 +0200

    finish

---
+++ LEV2_2/assets/js/main.js
@@ -0,0 +1,11 @@
+
+
+let vorname = document.getElementById("vorname");
+let nachname = document.getElementById("nachname");
+let land = document.getElementById("land");
+
+let myFunction = () => {
+    console.log(vorname.value);
+    console.log(nachname.value);
+    console.log(land.value);
+}
